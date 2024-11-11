a83a1865d6c10273f921ad4f9c9c24feec72c986
Tetiana Baranova <syniovych.tania@gmail.com>
2024-11-12 00:30:05 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+console.log(form);
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const elements = event.target.elements;
+    let formValid = true;
+    for (let element of elements) {
+        if (element.type !== "submit" && element.value.trim() === "") {
+            alert("All form fields must be filled in" + element.name);
+            formValid = false;
+        }
+    };
+    const info = {
+        email: elements.email.value.trim(),
+        password: elements.password.value.trim()
+    }
+    console.log(info);
+    event.target.reset();
+}
